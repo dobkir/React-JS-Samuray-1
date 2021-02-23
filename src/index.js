a44711e7f066f6ad68710997de0e59df7116ed4f
dobkir <dobkir@gmail.com>
2021-02-23 17:13:20 +0400

    render.js removed. Add observer pattern

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import reportWebVitals from './reportWebVitals';
-import { rerenderEntireTree } from './render';
-import state from './redux/state';
+import state, { subscriber } from './redux/state';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { addPost, updateNewPostText } from './redux/state';
+import { BrowserRouter } from 'react-router-dom';
+
+let rerenderEntireTree = (state) => {
+	ReactDOM.render(
+		<React.StrictMode>
+			<BrowserRouter>
+				<App state={state} addPost={addPost} updateNewPostText={updateNewPostText} />
+			</BrowserRouter>
+		</React.StrictMode>,
+		document.getElementById('root')
+	)
+}
+
 
 rerenderEntireTree(state);
-
+subscriber(rerenderEntireTree);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
