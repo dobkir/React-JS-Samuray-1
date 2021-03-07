d5d45ff6ff500e028c3131ceb44752573ea89655
dobkir <dobkir@gmail.com>
2021-03-07 23:19:53 +0400

    refactoring User.jsx into a class based component with using method componentDidMount() in axos get-request

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,12 +5,11 @@ import userPhoto from '../../images/defolt_user_64px.webp';
 
 class Users extends React.Component {
 
-	constructor(props) {
-		super(props);
+	componentDidMount() {
 		axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
 			this.props.setUsers(response.data.items);
 		});
-	}
+	};
 
 	render() {
 		return (
