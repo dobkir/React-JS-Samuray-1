cfea6997040709c4ae7965573018bbf27765dc59
dobkir <dobkir@gmail.com>
2021-03-08 19:06:12 +0400

    Pagination Users page

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -1,15 +1,14 @@
-import ava1 from '../images/ava-1.webp';
-
 const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET-USERS';
+const SET_CURRENT_PAGE = 'SET-CURRENT-PAGE';
+const SET_TOTAL_USERS_COUNT = 'SET-TOTAL-USERS-COUNT';
 
 let initialState = {
-	users: [
-		// { id: 1, avatarUrl: ava1, followed: false, fullName: 'Dmitry', status: 'I am like a boss', location: { city: 'Minsk', country: 'Belarus' } },
-		// { id: 2, avatarUrl: ava1, followed: true, fullName: 'Ivan', status: 'I am like a boss too', location: { city: 'Moskow', country: 'Russia' } },
-		// { id: 3, avatarUrl: ava1, followed: false, fullName: 'Andrew', status: 'I am like a boss too', location: { city: 'Minsk', country: 'Ukrain' } },
-	]
+	users: [],
+	pageSize: 5,
+	totalUsersCount: 0,
+	currentPage: 1
 };
 
 const usersReducer = (state = initialState, action) => {
@@ -37,8 +36,18 @@ const usersReducer = (state = initialState, action) => {
 		case SET_USERS:
 			return {
 				...state,
-				users: [...state.users, ...action.users]
-			}
+				users: action.users
+			};
+		case SET_CURRENT_PAGE:
+			return {
+				...state,
+				currentPage: action.currentPage
+			};
+		case SET_TOTAL_USERS_COUNT:
+			return {
+				...state,
+				totalUsersCount: action.count
+			};
 		default:
 			return state;
 	}
@@ -47,5 +56,7 @@ const usersReducer = (state = initialState, action) => {
 export const followAC = (userId) => ({ type: FOLLOW, userId });
 export const unfollowAC = (userId) => ({ type: UNFOLLOW, userId });
 export const setUsersAC = (users) => ({ type: SET_USERS, users });
+export const setCurrentPageAC = (currentPage) => ({ type: SET_CURRENT_PAGE, currentPage });
+export const setUsersTotalCountAC = (totalUsersCount) => ({ type: SET_TOTAL_USERS_COUNT, count: totalUsersCount });
 
 export default usersReducer;
