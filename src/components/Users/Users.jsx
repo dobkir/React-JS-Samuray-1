8513450a7295fa86dad7dadd851387fb3c285639
dobkir <dobkir@gmail.com>
2021-03-13 19:19:46 +0400

    Refactoring follow-onfollow callbacks in User component. Add to UsersContainer.jsx new parameters in get-requests

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,3 +1,4 @@
+import * as axios from "axios";
 import classes from './Users.module.css';
 import userPhoto from '../../images/defolt_user_64px.webp';
 import { NavLink } from 'react-router-dom';
@@ -32,8 +33,32 @@ let Users = (props) => {
 							</div>
 							<div>
 								{u.followed
-									? <button onClick={() => { props.unfollow(u.id) }}>Unfollow</button>
-									: <button onClick={() => { props.follow(u.id) }}>Follow</button>}
+									? <button onClick={() => {
+										axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {
+											withCredentials: true,
+											headers: {
+												"API-KEY": "6d42916e-ad19-4ca0-a218-14ad30106c58"
+											}
+										})
+											.then(response => {
+												if (response.data.resultCode == 0) {
+													props.unfollow(u.id);
+												}
+											});
+									}}>Unfollow</button>
+									: <button onClick={() => {
+										axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
+											withCredentials: true,
+											headers: {
+												"API-KEY": "6d42916e-ad19-4ca0-a218-14ad30106c58"
+											}
+										})
+											.then(response => {
+												if (response.data.resultCode == 0) {
+													props.follow(u.id);
+												}
+											});
+									}}>Follow</button>}
 							</div>
 						</span>
 						<span>
